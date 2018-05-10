51d14ea80e9599b23d893eb5935d2239cb18331b
Oliver Cahlander <Jesters@Olivers-MacBook-Air.local>
2018-05-10 04:09:02 -0700

    add jquery

---
+++ EquityStudy/js/buttonAction.js
@@ -0,0 +1,29 @@
+
+$("#boxPlot").click(function (){
+    $(".bodyImage").attr("src","images/BoxPlot_Chart_TSLA_GM_F_returns.png");
+});
+
+$("#cumulativeReturn").click(function (){
+    $(".bodyImage").attr("src", "images/Cumulative_Return_Chart_GM_F_TSLA_returns.png");    
+});
+
+$("#histogram").click(function (){
+    $(".bodyImage").attr("src","images/HistGram_100Chart_TSLA_GM_F.png");   
+});
+
+$("#kde").click(function (){
+    $(".bodyImage").attr("src","images/KDE_Chart_TSLA_GM_F.png");  
+});
+
+$("#openPrice").click(function (){
+    $(".bodyImage").attr("src","images/OpenPriceChart_TSLA.png");
+});
+
+$("#volume").click(function (){
+    $(".bodyImage").attr("src","images/VolumeChart_TSLA.png");
+});
+
+$("#scatter").click(function (){
+    $(".bodyImage").attr("src","images/Scatter_Chart_TSLA_GM_F_returns.png");
+});
+
